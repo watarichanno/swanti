948a170887861fa399e031bdaa6e19c1ce65e1bd
watarichanno <watarichanno@gmail.com>
2025-07-20 14:55:30 +0700

    Update dependencies

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def main():
     archive.save_to_archive()
     archive.read_from_archive()
     googleinterface.update_sheet()
-    graph.generate_img()
-    image.final_image()
+    #graph.generate_img()
+    #image.final_image()
     #imgurinterface.upload_image()
     dispatch.update_dispatch()
 
