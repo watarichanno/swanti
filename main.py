ba9f9c09c775998fa61b3b8d84ea3ec9ccc8f5bc
watarichanno <watarichanno@gmail.com>
2025-07-24 19:16:33 +0700

    Remove unused imports

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import dispatch
-import graph
-import image
 import archive
-import imgurinterface
 import googleinterface
 
 
@@ -10,9 +7,6 @@ def main():
     archive.save_to_archive()
     archive.read_from_archive()
     googleinterface.update_sheet()
-    #graph.generate_img()
-    #image.final_image()
-    #imgurinterface.upload_image()
     dispatch.update_dispatch()
 
 
